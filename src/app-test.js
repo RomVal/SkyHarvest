6ced8634f71167fc8c7d92d3875fe15be49f6229
Volodymyr <vpyshchyda@findmypast.com>
2024-11-23 10:30:51 +0200

    Update the debug command

--- src/app-test.js
+++ src/app-test.js
@@ -1,21 +1,29 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
 
-// Define the API URL
-const url = 'https://jsonplaceholder.typicode.com/posts/1';
+dotenv.config();
 
-// Function to make the GET request
-const fetchData = async () => {
+// URL to the service
+const URL = 'http://api.weatherapi.com/v1/current.json';
+
+export const getCurrentWeatherData = async (cityName) => {
+  const params = { key: process.env.WEATHER_API_KEY, q: cityName };
+  console.log('KEY', process.env.WEATHER_API_KEY);
   try {
-    // Make the GET request
-    const response = await axios.get(url);
+    const response = await axios.get(URL, { params });
 
-    // Log the response data
-    console.log('Response Data:', response.data);
+    if (response.status === 200) {
+      return response?.data;
+    } else {
+      console.log(`Error in getCurrentWeatherData`, JSON.stringify(response));
+    }
   } catch (error) {
-    // Log any errors
-    console.error('Error fetching data:', error.message);
+    console.log(
+      `Request failed in getCurrentWeatherData`,
+      JSON.stringify(error)
+    );
   }
 };
 
-// Call the function
-fetchData();
+const data = await getCurrentWeatherData('london');
+console.log(data);
