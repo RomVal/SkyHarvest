9b6bcc33f0fe690e70045a054ded9525c02b0cd5
Volodymyr <vpyshchyda@findmypast.com>
2024-11-24 23:23:57 +0200

    Update visualization script to plot traffic levels against formatted date and time

--- src/scripts/visual-city.py
+++ src/scripts/visual-city.py
@@ -4,13 +4,26 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('./../../users_london.csv')
 
-column_name = 'traffic_level'  # header name
+column_name = 'ID'  # header name
 array = data[column_name].dropna().to_list()  # COnverting to list removing NaN values
 
-# Showing the graph
-plt.plot(array)
-plt.title(f'Graph for "{column_name}"')
-plt.xlabel('Index')
-plt.ylabel('Value')
+column_values = 'traffic_level'  #  Y
+column_dates = 'ID'   # X
+
+# Take only days and hours
+data['formatted_date'] = pd.to_datetime(data[column_dates]).dt.strftime('%Y-%m-%d %H:%M')
+
+# Convert to list
+y_values = data[column_values].dropna().to_list()
+x_labels = data['formatted_date'].dropna().to_list()
+
+# Show the graph
+plt.figure(figsize=(10, 5))
+plt.plot(x_labels, y_values, marker='o') 
+plt.title(f'Графік даних зі стовпця "{column_values}"')
+plt.xlabel('Дата та час')
+plt.ylabel('Значення')
+plt.xticks(rotation=45, ha='right') 
 plt.grid()
+plt.tight_layout() 
 plt.show()
